ea787518c3c0d7f35b32c30cc916a4a4bd0bc61d
Peter Milo <pete.a.milo@gmail.com>
2022-09-28 21:00:43 -0400

    allow test script to override IP

--- javascript/test.js
+++ javascript/test.js
@@ -1,4 +1,11 @@
+const { lstat } = require('fs');
 const AdvatekV2 = require('./advatek');
+
+let pollAdr = '255.255.255.255';
+
+if(process.argv.length > 2) {
+    pollAdr = process.argv[2];
+}
 
 let adv = new AdvatekV2();
 
